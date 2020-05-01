0a319a43da8f61816d03b7e9959adc3c888e2cd4
maki_yuya <halo.leo77@gmail.com>
2020-05-01 18:55:14 +0900

    [add] Initialize repository

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,62 @@
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @test = "テストテキスト"
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @answer = Answer.new
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    if @question.save
+      redirect_to("/questions/#{@question.id}")
+    else
+      render("questions/new")
+    end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      flash[:notice] = "成功！"
+      redirect_to("/questions/#{@question.id}")
+    else
+      flash.now[:alert] = "失敗！"
+      render("questions/edit")
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    flash[:notice] = "成功！"
+    redirect_to("/questions")
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :body)
+    end
+
+    def ensure_correct_user
+      @question = Question.find_by(id: params[:id])
+      if @question.user_id != current_user.id
+        flash[:alert] = "権限がありません"
+        redirect_to("/questions/#{@question.id}")
+      end
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  get "/users/:id", to: "users#show"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'questions#index'
+  resources :questions do
+    resources :answers, only: [:create]
+  end
+end
